739211cbff405c8a0bba96acd5fcc6538bbe9a55
dbonafin <demianbonafina@gmail.com>
2022-06-17 16:42:42 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Viene fornito un layout di base in cui è presente una card di esempio inserita nell’html. 
+// Questa card serve solo come esempio e dovrà quindi essere rimossa dall’html.
+// Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. 
+// Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// Una volta definita la struttura dati, stampare dinamicamente una card per ogni membro del team.
+
+console.log("test");
