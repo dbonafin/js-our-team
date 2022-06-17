155c53046017945840c916b938c5664ef419c13f
dbonafin <demianbonafina@gmail.com>
2022-06-17 17:50:59 +0200

    team member cards done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
 // Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 // Una volta definita la struttura dati, stampare dinamicamente una card per ogni membro del team.
 
+
+// Create the array that contains all the details of the member
 const membersArray = [
     {
         name: "Wayne",
@@ -16,10 +18,34 @@ const membersArray = [
         lastname: "Caroll",
         role: "Chief Editor",
         image: `<img src="img/angela-caroll-chief-editor.jpg" alt="Angela Caroll"</>`
-    }
+    },
+    {
+        name: "Walter",
+        lastname: "Gordon",
+        role: "Office Manager",
+        image: `<img src="img/walter-gordon-office-manager.jpg" alt="Walter Gordon"</>`
+    },
+    {
+        name: "Angela",
+        lastname: "Lopez",
+        role: "Social Media Manager",
+        image: `<img src="img/angela-lopez-social-media-manager.jpg" alt="Angela Lopez"</>`
+    },
+    {
+        name: "Scott",
+        lastname: "Estrada",
+        role: "Developer",
+        image: `<img src="img/scott-estrada-developer.jpg" alt="Scott Estrada"</>`
+    },
+    {
+        name: "Barbara",
+        lastname: "Ramos",
+        role: "Graphic Designer",
+        image: `<img src="img/barbara-ramos-graphic-designer.jpg" alt="Barbara Ramos"</>`
+    },
 ];
 
-
+// Select the container that will contain the member-cards
 const teamContainer = document.querySelector('.team-container');
 
 // For each member create a dynamic card
@@ -27,22 +53,16 @@ for(let i = 0; i < membersArray.length; i++) {
     const thisMember = membersArray[i];
     console.log(thisMember);
 
-    // Create the card with the member details
+    // Create the card with the different-member details
     const memberCard = `
-    <div class="single-class">
-        <div>${thisMember.image}</div>
-        <h3>${thisMember.name} ${thisMember.lastname}</h2>
-        <div>${thisMember.role}</div>
-    </div>
+    <div class="single-class team-card">
+        <div class="card-image">${thisMember.image}</div>
+        <div class="card-text">
+            <h3>${thisMember.name} ${thisMember.lastname}</h2>
+            <div>${thisMember.role}</div></div>
+        </div>
     `;
 
-    // Select the member-div and add the css style
-    // const detailsTeamMember = document.querySelector(".single-class");
-    // console.log(detailsTeamMember);
-
-    // detailsTeamMember.classList.add("team-card");
-    
     // Append the team-member-card to the teamContainer in the DOM
     teamContainer.innerHTML += memberCard;
-
 }
